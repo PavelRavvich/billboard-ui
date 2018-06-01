59f89b5bd5362c41aaaa4069cfd91645d08f2ee4
Pavel <triodjangopiter@yandex.ru>
2018-06-01 15:16:04 +0300

    import ref

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 import Vuetify from 'vuetify'
-import store from './store'
 import 'vuetify/dist/vuetify.min.css'
 import * as fb from 'firebase'
+import router from './router'
+import store from './store'
+import App from './App'
 
 Vue.use(Vuetify)
 

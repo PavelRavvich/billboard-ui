7c32e260c085b3ad04e96b26afc1d31ded720728
Pavel <triodjangopiter@yandex.ru>
2018-06-03 15:31:04 +0300

    add upload image for new ad

--- src/store/ads.js
+++ src/store/ads.js
@@ -38,9 +38,11 @@ export default {
         )
         const ad = await fb.database().ref('ads').push(newAd)
         const imageExt = image.name.slice(image.name.lastIndexOf('.'))
-        const storeRef = fb.storage().ref(`ads/${ad.key + imageExt}`)
+        const imgPath = `ads/${ad.key + imageExt}`
+        const storeRef = fb.storage().ref(imgPath)
         await storeRef.put(image).then(resolve => {}, reject => {})
         await storeRef.getDownloadURL().then(imageSrc => {
+          newAd.imageSrc = imageSrc
           commit('setLoading', false)
           commit('createAd', {
             ...newAd,
@@ -48,6 +50,7 @@ export default {
             imageSrc
           })
         }).catch(err => console.log(err))
+        await fb.database().ref('ads').child(ad.key).update({imageSrc: newAd.imageSrc})
       } catch (error) {
         console.log(error)
         commit('setError', error.message)
